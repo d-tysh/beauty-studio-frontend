aca1c5e88d50f1056480983ff67dc09f5e71fdcb
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-09-15 11:42:15 +0300

    Add RestrictedRoute component

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,14 +4,21 @@ import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
 import { AdminInfoPage } from './pages/AdminInfoPage'
+import { RestrictedRoute } from './components/RestrictedRoute'
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/' element={<Layout />}>
                 <Route index element={<HomePage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route path='/register' element={<RegisterPage />} />
+                <Route 
+                    path='/login' 
+                    element={<RestrictedRoute redirectTo='/' component={<LoginPage />} />} 
+                />
+                <Route 
+                    path='/register' 
+                    element={<RestrictedRoute redirectTo='/' component={<RegisterPage />} />} 
+                />
                 <Route path='/admins' element={<h2 className='custom-h2'>Admins</h2>} />
                 <Route path='/clients' element={<h2 className='custom-h2'>Clients</h2>} />
                 <Route path='/procedures' element={<h2 className='custom-h2'>Procedures</h2>} />
