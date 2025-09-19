33f0cb58719f0315ad83d8e5cd58c41508918e7f
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-09-19 15:49:14 +0300

    Add components (AdminsList, AdminsListItem)

--- src/pages/AllAdminsPage.tsx
+++ src/pages/AllAdminsPage.tsx
@@ -3,8 +3,9 @@ import { useGetAllAdminsQuery } from "../api/adminApi"
 import { Loader } from "../components/Loader";
 import { selectAdmin } from "../redux/admin/selectors";
 import { useAppSelector } from "../redux/hooks";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { H2 } from "../components/headers/H2";
+import { AdminsList } from "../components/adminComponents/adminsList";
 
 export const AllAdminsPage = () => {
     const { data, isLoading, error } = useGetAllAdminsQuery();
@@ -24,33 +25,7 @@ export const AllAdminsPage = () => {
                 {isLoading && <Loader />}
                 {error && !isLoading && <p>Error! Something went wrong...</p>}
                 {
-                    !error && !isLoading && currentAdmin?.status === 'pro' && data &&
-                    <ul className="flex gap-4 flex-col">
-                        {
-                            data.result.map(admin =>
-                                <li key={admin._id} className="servise-list-item">
-                                    <Link to={`/admin/${admin._id}`}>
-                                        <table className="servise-table">
-                                            <tbody>
-                                                <tr>
-                                                    <td>Name</td>
-                                                    <td>{admin.name}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Login</td>
-                                                    <td>{admin.login}</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Status</td>
-                                                    <td>{admin.status}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </Link>
-                                </li>
-                            )
-                        }
-                    </ul>
+                    !error && !isLoading && currentAdmin?.status === 'pro' && data && <AdminsList admins={data.result} />
                 }
             </div>
         </div>
