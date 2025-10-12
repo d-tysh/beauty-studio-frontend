d40b66894c6ec361f99f508a6a99a29a5bd8b204
d-tysh <39271488+d-tysh@users.noreply.github.com>
2025-10-12 18:25:42 +0300

    Update ServicesSelect

--- src/components/procedureComponents/ServicesSelect.tsx
+++ src/components/procedureComponents/ServicesSelect.tsx
@@ -7,23 +7,19 @@ export const ServicesSelect = () => {
     });
 
     return (
-        <label className="custom-form-label">
+        <div className="custom-form-label">
             <span className="font-semibold w-1/3 ml-2 md:ml-0">Services</span>
             <div className="custom-form-field flex flex-col max-h-50 overflow-y-auto">
                 {isServicesError && <p>Unable to load services...</p>}
                 {
                     servicesData && servicesData.data.map(service =>
-                        <label key={service._id}
-                            className="flex gap-2 items-center not-last-of-type:border-b-1 py-2"
-                        >
-                            <Field
-                                type="checkbox" name="services" value={service._id}
-                            />
+                        <label key={service._id} className="flex gap-2 items-center not-last-of-type:border-b-1 py-2">
+                            <Field type="checkbox" name="services" value={service._id} />
                             {service.serviceName}, price {service.price}, time {service.time}
                         </label>
                     )
                 }
             </div>
-        </label>
+        </div>
     )
 }
